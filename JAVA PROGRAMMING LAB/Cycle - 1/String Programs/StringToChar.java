221aa660b2e24157ac3c246721762edfc8d8164a
Lalitha devi Thurlapati <85609593+Lalitha12b1-T@users.noreply.github.com>
2021-07-12 10:30:17 +0530

    Add files via upload

---
+++ JAVA PROGRAMMING LAB/Cycle - 1/String Programs/StringToChar.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class StringToChar
+{
+   public static void main(String args[])
+   {
+      // Using charAt() method
+      String str = "Hello";
+      for(int i=0; i<str.length();i++){
+        char ch = str.charAt(i);
+        System.out.println("Character at "+i+" Position: "+ch);
+      } 
+   }
+}
